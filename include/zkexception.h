2cf2c1bb0d7b5d41ffaed4dd7dfb6087cc246eef
davislee168 <53437101+davislee168@users.noreply.github.com>
2020-04-30 17:11:03 -0400

    Update zkexception.h

--- include/zkexception.h
+++ include/zkexception.h
@@ -1,61 +1,57 @@
-******************************************************
-?
-zkexception.h
 #ifndef _ZKEXCEPTION_H
 #define _ZKEXCEPTION_H
-?
+
 #include "zookeeper.h"
 #include <string>
 #include <stdexcept>
-?
+
 using namespace std;
-?
+
 class zkException:public std::out_of_range
 {
 public:
-zkException(string &msg):out_of_range(msg)
-{
-m_msg = msg;
-}
-zkException(int code, string &msg):out_of_range(msg)
-{
-m_errCode = code;
-m_msg = msg;
-}
-virtual ~zkException() throw(){};
-?
+  zkException(string &msg):out_of_range(msg)
+  {
+    m_msg = msg;
+  }
+  zkException(int code, string &msg):out_of_range(msg)
+  {
+    m_errCode = code;
+    m_msg = msg;
+  }
+  virtual ~zkException() throw(){};
+
 private:
-string m_msg;
-int m_errCode;
+  string m_msg;
+  int m_errCode;
 };
-?
+
 class zkExceptionLossConnection: public zkException
 {
 public:
-zkExceptionLossConnection(int code, string &msg):zkException(msg){}
-~zkExceptionLossConnection() throw(){};
+  zkExceptionLossConnection(int code, string &msg):zkException(msg){}
+  ~zkExceptionLossConnection() throw(){};
 };
-?
+
 class zkExceptionNoNode: public zkException
 {
 public:
-zkExceptionNoNode(int code, string &msg):zkException(msg){}
-~zkExceptionNoNode() throw(){};
+  zkExceptionNoNode(int code, string &msg):zkException(msg){}
+  ~zkExceptionNoNode() throw(){};
 };
-?
+
 class zkExceptionNodeExists: public zkException
 {
 public:
-zkExceptionNodeExists(int code, string &msg):zkException(msg){}
-~zkExceptionNodeExists() throw(){};
+  zkExceptionNodeExists(int code, string &msg):zkException(msg){}
+  ~zkExceptionNodeExists() throw(){};
 };
-?
+
 class zkExceptionSessionExpired: public zkException
 {
 public:
-zkExceptionSessionExpired(int code, string &msg):zkException(msg){}
-~zkExceptionSessionExpired() throw(){};
+  zkExceptionSessionExpired(int code, string &msg):zkException(msg){}
+  ~zkExceptionSessionExpired() throw(){};
 };
-?
-#endif // _ZKEXCEPTION_H
 
+#endif // _ZKEXCEPTION_H
