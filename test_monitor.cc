829f8a486345898de0dfb677b2e14c4787f136f8
davislee168 <53437101+davislee168@users.noreply.github.com>
2020-04-30 14:38:36 -0400

    Add files via upload

---
+++ test_monitor.cc
@@ -0,0 +1,37 @@
+***********************************************************
+?
+test_monitor.cpp
+#include "zkclient.h"
+#include "zkdlock.h"
+#include <iostream>
+#include <string>
+#include <list>
+?
+using namespace std;
+?
+int main()
+{
+// string hostname = "delfdta9awsc:2177";
+string hostname = 
+"113.140.207.132:2177,113.140.207.133:2177,113.140.207.134:2177";
+string lockpath = "/test1";
+string lockname = "dlock";
+long sessionTimeout = 10000;
+bool watchPrecedingNode = false;
+?
+// get "/lock" to m_dLockPath, assign "dlock" to m_dLockName
+// connect to zookeeper and create persistent "/test1" node
+// zkdlock dlock(hostname, lockname);
+zkdlock dlock(hostname, lockpath, lockname, watchPrecedingNode);
+// dlock.construct_zkclient(hostname, sessionTimeout, 60);
+?
+// find a pid which holds a key
+bool monitor = dlock.dlockMonitor();
+?
+// disconnect
+// dlock.getZkclient()->close();
+dlock.close();
+std::cout << "disconnected" << std::endl;
+return 0;
+}
+
