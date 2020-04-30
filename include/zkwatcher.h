f498232bdfedab9bbade379951e3445d9c7eb5a7
davislee168 <53437101+davislee168@users.noreply.github.com>
2020-04-30 17:17:51 -0400

    Update zkwatcher.h

--- include/zkwatcher.h
+++ include/zkwatcher.h
@@ -1,25 +1,23 @@
-*******************************************************
-?
-zkwatcher.h#ifndef _ZKWATCHER_H
+#ifndef _ZKWATCHER_H
 #define _ZKWATCHER_H
-?
+
 #include <string>
 #include "zkconnect.h"
-?
+
 class zkwatcher
 {
 public:
-zkwatcher();
-virtual ~zkwatcher();
-?
-// thread function
-void run();
-bool start();
-?
-bool isShutDown();
-?
-virtual void process() = 0;
+  zkwatcher();
+  virtual ~zkwatcher();
+
+  // thread function
+  void run();
+  bool start();
+
+  bool isShutDown();
+
+  virtual void process() = 0;
 };
-?
+
 #endif // _ZKWATCHER_H
 
