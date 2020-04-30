064e3787b9b69c006c4113491f4c2ac3e70ac8ec
davislee168 <53437101+davislee168@users.noreply.github.com>
2020-04-30 14:35:05 -0400

    Update test_dlock.cc

--- test_dlock.cc
+++ test_dlock.cc
@@ -1,49 +1,45 @@
-**************************************************************
-test_dlock.gpp
-?
 #include "zkclient.h"
 #include "zkdlock.h"
 #include <iostream>
 #include <string>
 #include <list>
-?
+
 using namespace std;
-?
+
 int main()
 {
-// string hostname = "delfdta9awsc:2177";
-string hostname = "113.140.207.132:2177,113.140.207.133:2177,113.140.207.134:2177";
-string lockpath = "/test1";
-string lockname = "dlock";
-long sessionTimeout = 10000;
-bool watchPrecedingNode = false;
-?
-// get "/lock" to m_dLockPath, assign "dlock" to m_dLockName
-// connect to zookeeper and create persistent "/test1" node
-// zkdlock dlock(hostname);
-// zkdlock dlock(hostname, watchPrecedingNode);
-// zkdlock dlock(hostname, lockname, watchPrecedingNode);
-zkdlock dlock(hostname, lockpath, lockname, watchPrecedingNode);
-// dlock.construct_zkclient(hostname, sessionTimeout, 60);
-?
-// test distributed lock
-while (dlock.dlock()) {
-?
-std::cout << "*** Obtain a key, do OqsManager ***" << std::endl;
-?
-// do process
-while (true) {
-// sleep(300);
-if (dlock.getZkclient() == NULL) {
-std::cout << "Connection broken ..." << std::endl;
-break;
-}
-}
-}
-?
-// disconnect
-// dlock.getZkclient()->close();
-dlock.close();
-std::cout << "disconnected" << std::endl;
-return 0;
+  // string hostname = "delfdta9awsc:2177";
+  string hostname = "113.140.207.132:2177,113.140.207.133:2177,113.140.207.134:2177";
+  string lockpath = "/test1";
+  string lockname = "dlock";
+  long sessionTimeout = 10000;
+  bool watchPrecedingNode = false;
+
+  // get "/lock" to m_dLockPath, assign "dlock" to m_dLockName
+  // connect to zookeeper and create persistent "/test1" node
+  // zkdlock dlock(hostname);
+  // zkdlock dlock(hostname, watchPrecedingNode);
+  // zkdlock dlock(hostname, lockname, watchPrecedingNode);
+  zkdlock dlock(hostname, lockpath, lockname, watchPrecedingNode);
+  // dlock.construct_zkclient(hostname, sessionTimeout, 60);
+
+  // test distributed lock
+  while (dlock.dlock()) {
+    std::cout << "*** Obtain a key, do OqsManager ***" << std::endl;
+
+    // do process
+    while (true) {
+      // sleep(300);
+      if (dlock.getZkclient() == NULL) {
+        std::cout << "Connection broken ..." << std::endl;
+        break;
+      }
+    }
+  }
+
+  // disconnect
+  // dlock.getZkclient()->close();
+  dlock.close();
+  std::cout << "disconnected" << std::endl;
+  return 0;
 }
